ef4182798dfa61dc916c612051b5f0277e2b34b1
alex.li6 <alex.li@snaptravel.com>
2022-10-30 13:35:52 -0400

    updated eslint + node packages config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const withTM = require("next-transpile-modules")(["three"])
+
+module.exports = withTM(nextConfig)
